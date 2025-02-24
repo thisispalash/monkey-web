fde97d55214af2801d839e860517ea4890284727
isthispalash <thisispalash@kdio.xyz>
2025-02-24 13:52:57 -0700

    connect wallet works!

--- src/context/Web3Context.tsx
+++ src/context/Web3Context.tsx
@@ -4,8 +4,34 @@ import React, { type ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
-import { wagmiAdapter } from '@/lib/web3/wagmi'
-import { modal, metadata } from '@/lib/web3/appkit'
+import { createAppKit } from '@reown/appkit'
+
+import { wagmiAdapter, projectId, networks } from '@/lib/web3/wagmi'
+import { rsktestnet } from '@/lib/web3/chains/RootstockTestnet';
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+// Set up metadata
+const metadata = {
+  name: 'Dash Mon[k]ey',
+  description: 'Monkey see, Monkey do!',
+  url: 'http://localhost:3000', // origin must match your domain & subdomain
+  icons: ['/circus-monkey.svg']
+}
+
+// Create the modal
+export const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks,
+  defaultNetwork: rsktestnet,
+  metadata: metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+});
 
 // Set up queryClient
 const queryClient = new QueryClient()
--- src/lib/web3/wagmi.ts
+++ src/lib/web3/wagmi.ts
@@ -1,5 +1,6 @@
 import { cookieStorage, createStorage } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import type { AppKitNetwork } from '@reown/appkit/networks';
 
 import { rsktestnet } from './chains/RootstockTestnet';
 
@@ -10,7 +11,7 @@ if (!projectId) {
   throw new Error('Project ID is not defined');
 }
 
-export const networks = [rsktestnet]
+export const networks = [rsktestnet] as [AppKitNetwork, ...AppKitNetwork[]];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
