1ebe031ff55bd96e0588e64b8302288a00417462
isthispalash <thisispalash@kdio.xyz>
2025-02-25 13:12:28 -0700

    add erc20 abi

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -4,9 +4,12 @@ import RecipeFactory from './RecipeFactory.json';
 import Monkey from './Monkey.json';
 import Recipe from './Recipe.json';
 
+import ERC20 from './ERC20.json';
+
 export {
   MonkeyFactory,
   RecipeFactory,
   Monkey,
   Recipe,
+  ERC20,
 }
